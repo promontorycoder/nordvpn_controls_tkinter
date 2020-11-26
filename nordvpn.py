635257a51b83efc881075792841639348fb848cf
promontorycoder <promontorycoder@tutanota.com>
2020-11-26 16:26:03 -0500

    Improved GUI

--- nordvpn.py
+++ nordvpn.py
@@ -9,7 +9,7 @@ import time
 root = Tk()
 
 # Define GUI
-root.geometry('800x500')
+root.geometry('1000x950')
 root.configure(bg='gray7')
 root.resizable(1,1)
 root.title("NordVPN")
@@ -19,76 +19,164 @@ Label(root, text = "NordVPN Controls",
     font = 'arial 16 bold', bg='gray7', fg='lime green').pack()
 
 
+def clear_output():
+
+    text_output_window.delete(1.0, END)
+
+
+def get_info(arg):
+
+    print(text_output_window.get("1.0", "current lineend"))
+
+
 def vpn_status():
-    nord_stat = "nordvpn status"
-    os.system(nord_stat)
+   
+    clear_output()
+    
+    f = os.popen('nordvpn status')
+    for line in f:
+        line = line.strip()
+        if line:
+            text_output_window.insert("end", line + '\n')
+    text_output_window.bind("<Return>", get_info)
     
 
 def vpn_settings():
-    nord_set = "nordvpn settings"
-    os.system(nord_set)
+        
+    clear_output()
+    
+    f = os.popen('nordvpn settings')
+    for line in f:
+        line = line.strip()
+        if line:
+            text_output_window.insert("end", line + '\n')
+    text_output_window.bind("<Return>", get_info)
     
     
 def vpn_protocol_udp():
-    nordvpn_protocol_udp = "nordvpn set protocol udp"
-    os.system(nordvpn_protocol_udp)
+       
+    f = os.popen('nordvpn set protocol udp')
+    for line in f:
+        line = line.strip()
+        if line:
+            text_output_window.insert("end", line + '\n')
+    text_output_window.bind("<Return>", get_info)
     
     
 def vpn_protocol_tcp():
-    nordvpn_protocol_tcp = "nordvpn set protocol tcp"
-    os.system(nordvpn_protocol_tcp)
+        
+    f = os.popen('nordvpn set protocol tcp')
+    for line in f:
+        line = line.strip()
+        if line:
+            text_output_window.insert("end", line + '\n')
+    text_output_window.bind("<Return>", get_info)
     
     
 def vpn_ks_on():
-    nord_ks_on = "nordvpn set killswitch enabled"
-    os.system(nord_ks_on)
+
+    f = os.popen("nordvpn set killswitch enabled")
+    for line in f:
+        line = line.strip()
+        if line:
+            text_output_window.insert("end", line + '\n')
+    text_output_window.bind("<Return>", get_info)
     
     
 def vpn_ks_off():
-    nord_ks_off = "nordvpn set killswitch disabled"
-    os.system(nord_ks_off)
     
+    f = os.popen('nordvpn set killswitch disabled')
+    for line in f:
+        line = line.strip()
+        if line:
+            text_output_window.insert("end", line + '\n')
+    text_output_window.bind("<Return>", get_info)
+   
     
 def vpn_cybsec_off():
-    nordvpn_cybersec_off = "nordvpn set cybersec disabled"
-    os.system(nordvpn_cybersec_off)
+    
+    f = os.popen("nordvpn set cybersec disabled")
+    for line in f:
+        line = line.strip()
+        if line:
+            text_output_window.insert("end", line + '\n')
+    text_output_window.bind("<Return>", get_info)
     
     
 def vpn_cybsec_on():
-    nordvpn_cybersec_on = "nordvpn set cybersec enabled"
-    os.system(nordvpn_cybersec_on)
+    
+    f = os.popen('nordvpn set cybersec enabled')
+    for line in f:
+        line = line.strip()
+        if line:
+            text_output_window.insert("end", line + '\n')
+    text_output_window.bind("<Return>", get_info)
     
     
 def vpn_obsf_off():
-    nordvpn_obfuscate_off = "nordvpn set obfuscate disabled"
-    os.system(nordvpn_obfuscate_off)
+
+    f = os.popen('nordvpn set obfuscate disabled')
+    for line in f:
+        line = line.strip()
+        if line:
+            text_output_window.insert("end", line + '\n')
+    text_output_window.bind("<Return>", get_info)
     
     
 def vpn_obsf_on():
-    nordvpn_obfuscate_on = "nordvpn set obfuscate enabled"
-    os.system(nordvpn_obfuscate_on)
+    
+    f = os.popen("nordvpn set obfuscate enabled")
+    for line in f:
+        line = line.strip()
+        if line:
+            text_output_window.insert("end", line + '\n')
+    text_output_window.bind("<Return>", get_info)
     
     
 def vpn_notify_off():
-    nordvpn_notify_off = "nordvpn set notify disabled"
-    os.system(nordvpn_notify_off)
+    
+    f = os.popen("nordvpn set notify disabled")
+    for line in f:
+        line = line.strip()
+        if line:
+            text_output_window.insert("end", line + '\n')
+    text_output_window.bind("<Return>", get_info)
     
     
 def vpn_notify_on():
-    nordvpn_notify_on = "nordvpn set notify enabled"
-    os.system(nordvpn_notify_on)
+
+    f = os.popen("nordvpn set notify enabled")
+    for line in f:
+        line = line.strip()
+        if line:
+            text_output_window.insert("end", line + '\n')
+    text_output_window.bind("<Return>", get_info)
     
     
 def vpn_ac_off():
-    nordvpn_ac_off = "nordvpn set autoconnect disabled"
-    os.system(nordvpn_ac_off)
+    
+    f = os.popen("nordvpn set autoconnect disabled")
+    for line in f:
+        line = line.strip()
+        if line:
+            text_output_window.insert("end", line + '\n')
+    text_output_window.bind("<Return>", get_info)
     
     
 def vpn_ac_on():
-    nordvpn_ac_on = "nordvpn set autoconnect enabled"
-    os.system(nordvpn_ac_on)
+
+    f = os.popen("nordvpn set autoconnect enabled")
+    for line in f:
+        line = line.strip()
+        if line:
+            text_output_window.insert("end", line + '\n')
+    text_output_window.bind("<Return>", get_info)
+    
     
 def vpn_c():
+    
+    clear_output()
+    
     vpn_c_us()
     time.sleep(2)
     
@@ -104,185 +192,269 @@ def vpn_c():
     vpn_cybsec_off()
     time.sleep(1)
     
-    vpn_settings()
-    vpn_status()
+    f = os.popen('nordvpn status')
+    for line in f:
+        line = line.strip()
+        if line:
+            text_output_window.insert("end", line + '\n')
+    text_output_window.bind("<Return>", get_info)
     
-        
+    
 def vpn_c_us():
-    nord_conn_us = "nordvpn c us"
-    os.system(nord_conn_us)
+    
+    clear_output()
+    
+    f = os.popen("nordvpn c us")
+    for line in f:
+        line = line.strip()
+        if line:
+            text_output_window.insert("end", line + '\n')
+    text_output_window.bind("<Return>", get_info)
     
     
 def vpn_c_chicago():
-    nord_conn_chicago = "nordvpn c chicago"
-    os.system(nord_conn_chicago)
+
+    clear_output()
+    
+    f = os.popen("nordvpn c chicago")
+    for line in f:
+        line = line.strip()
+        if line:
+            text_output_window.insert("end", line + '\n')
+    text_output_window.bind("<Return>", get_info)
     
     
 def vpn_c_new_york():
-    nord_conn_new_york = "nordvpn c new_york"
-    os.system(nord_conn_new_york)
+    
+    clear_output()
+    
+    f = os.popen("nordvpn c new_york")
+    for line in f:
+        line = line.strip()
+        if line:
+            text_output_window.insert("end", line + '\n')
+    text_output_window.bind("<Return>", get_info)
     
     
 def vpn_c_miami():
-    nord_conn_miami = "nordvpn c miami"
-    os.system(nord_conn_miami)
+    
+    clear_output()
+
+    f = os.popen("nordvpn c miami")
+    for line in f:
+        line = line.strip()
+        if line:
+            text_output_window.insert("end", line + '\n')
+    text_output_window.bind("<Return>", get_info)
     
 
 def vpn_c_canada():
-    nord_conn_ca = "nordvpn c canada"
-    os.system(nord_conn_ca)
+    
+    clear_output()
+    
+    f = os.popen("nordvpn c canada")
+    for line in f:
+        line = line.strip()
+        if line:
+            text_output_window.insert("end", line + '\n')
+    text_output_window.bind("<Return>", get_info)
     
 
-def vpn_c_france():
-    nord_conn_france = "nordvpn c france"
-    os.system(nord_conn_france)
-    
-    
-def vpn_c_germany():
-    nord_conn_germany = "nordvpn c germany"
-    os.system(nord_conn_germany)
-    
-    
 def vpn_c_uk():
-    nord_conn_uk = "nordvpn c uk"
-    os.system(nord_conn_uk)
+    
+    clear_output()
+    
+    f = os.popen("nordvpn c uk")
+    for line in f:
+        line = line.strip()
+        if line:
+            text_output_window.insert("end", line + '\n')
+    text_output_window.bind("<Return>", get_info)
+    
+
+def vpn_d():
+    
+    clear_output()
+    
+    f = os.popen("nordvpn d")
+    for line in f:
+        line = line.strip()
+        if line:
+            text_output_window.insert("end", line + '\n')
+    text_output_window.bind("<Return>", get_info)
+    
+
+def vpn_rate():
+    
+    f = os.popen("nordvpn rate 5")
+    for line in f:
+        line = line.strip()
+        if line:
+            text_output_window.insert("end", line + '\n')
+    text_output_window.bind("<Return>", get_info)
 
     
 def vpn_disconnect():
-    nord_ks_off = "nordvpn set killswitch disabled"
-    nordvpn_ac_off = "nordvpn set autoconnect disabled"
-    nordvpn_whitelist_off = "nordvpn whitelist remove port 8080"
-    nord_conn_off = "nordvpn d"
-    nord_rate = "nordvpn rate 5"
+
+    clear_output()
     
-    os.system(nord_ks_off)
+    vpn_ks_off()
     time.sleep(2)
-    os.system(nordvpn_ac_off)
+    
+    vpn_ac_off()
     time.sleep(2)
-    os.system(nordvpn_whitelist_off)
+    
+    vpn_wl_off()
     time.sleep(2)
-    os.system(nord_conn_off)
+    
+    vpn_d()
     time.sleep(2)
-    os.system(nord_rate)
+    
+    vpn_rate()
     time.sleep(1)
     
-    vpn_settings()
-    vpn_status()
+    g = os.popen('nordvpn settings')
+    for line in g:
+        line = line.strip()
+        if line:
+            text_output_window.insert("end", line + '\n')
+    text_output_window.bind("<Return>", get_info)
+    
+    f = os.popen('nordvpn status')
+    for line in f:
+        line = line.strip()
+        if line:
+            text_output_window.insert("end", line + '\n')
+    text_output_window.bind("<Return>", get_info)
     
     
 def vpn_wl_on():
-    nordvpn_whitelist = "nordvpn whitelist add port 8080"
-    os.system(nordvpn_whitelist)
+
+    f = os.popen("nordvpn whitelist add port 8080")
+    for line in f:
+        line = line.strip()
+        if line:
+            text_output_window.insert("end", line + '\n')
+    text_output_window.bind("<Return>", get_info)
     
     
 def vpn_wl_off():
-    nordvpn_whitelist_off = "nordvpn whitelist remove port 8080"
-    os.system(nordvpn_whitelist_off)
     
-
-def clear_terminal():
-    clear = "clear"
-    os.system(clear)
+    f = os.popen("nordvpn whitelist remove port 8080")
+    for line in f:
+        line = line.strip()
+        if line:
+            text_output_window.insert("end", line + '\n')
+    text_output_window.bind("<Return>", get_info)
     
+   
 def Exit():
     exit()
 
+
+# Create Buttons
 B1 = Button(root, font = 'arial 10 bold', text = "VPN Status", 
-    padx=2, bg='cyan', command=vpn_status).place(x=60, y=50)
+    padx=2, bg='cyan', command=vpn_status).place(x=60, y=170)
     
 B2 = Button(root, font = 'arial 10 bold', text = "VPN Settings", 
-    padx=2, bg='cyan', command=vpn_settings).place(x=60, y=90)
+    padx=2, bg='cyan', command=vpn_settings).place(x=60, y=210)
 
     
 B3 = Button(root, font = 'arial 10 bold', text = "VPN Protocol UDP", 
-    padx=2, bg='plum1', command=vpn_protocol_udp).place(x=600, y=50)
+    padx=2, bg='plum1', command=vpn_protocol_udp).place(x=500, y=50)
     
 B4 = Button(root, font = 'arial 10 bold', text = "VPN Protocol TCP", 
-    padx=2, bg='plum1', command=vpn_protocol_tcp).place(x=600, y=90)
+    padx=2, bg='plum1', command=vpn_protocol_tcp).place(x=500, y=90)
 
     
 B5 = Button(root, font = 'arial 10 bold', text = "Killswitch Disable", 
-    padx=2, bg='SkyBlue2', command=vpn_ks_off).place(x=400, y=170)
+    padx=2, bg='MediumOrchid1', command=vpn_ks_off).place(x=350, y=130)
 
 B6 = Button(root, font = 'arial 10 bold', text = "Killswitch Enable", 
-    padx=2, bg='SkyBlue2', command=vpn_ks_on).place(x=400, y=210)
+    padx=2, bg='MediumOrchid1', command=vpn_ks_on).place(x=350, y=170)
 
     
 B7 = Button(root, font = 'arial 10 bold', text = "CyberSec Disable", 
-    padx=2, bg='plum2', command=vpn_cybsec_off).place(x=600, y=170)
+    padx=2, bg='MediumOrchid1', command=vpn_cybsec_off).place(x=500, y=130)
 
 B8 = Button(root, font = 'arial 10 bold', text = "CyberSec Enable", 
-    padx=2, bg='plum2', command=vpn_cybsec_on).place(x=600, y=210)
+    padx=2, bg='MediumOrchid1', command=vpn_cybsec_on).place(x=500, y=170)
 
 
 B9 = Button(root, font = 'arial 10 bold', text = "Obfuscate Off", 
-    padx=2, bg='SkyBlue3', command=vpn_obsf_off).place(x=400, y=290)
+    padx=2, bg='purple1', command=vpn_obsf_off).place(x=350, y=210)
 
 B10 = Button(root, font = 'arial 10 bold', text = "Obfuscate On", 
-    padx=2, bg='SkyBlue3', command=vpn_obsf_on).place(x=400, y=330)
+    padx=2, bg='purple1', command=vpn_obsf_on).place(x=350, y=250)
 
 
 B11 = Button(root, font = 'arial 10 bold', text = "Notifications Off", 
-    padx=2, bg='plum3', command=vpn_notify_off).place(x=600, y=290)
+    padx=2, bg='purple1', command=vpn_notify_off).place(x=500, y=210)
 
 B12 = Button(root, font = 'arial 10 bold', text = "Notificaions On", 
-    padx=2, bg='plum3', command=vpn_notify_on).place(x=600, y=330)
+    padx=2, bg='purple1', command=vpn_notify_on).place(x=500, y=250)
 
 
 B13 = Button(root, font = 'arial 10 bold', text = "Auto-Connect Off", 
-    padx=2, bg='SkyBlue1', command=vpn_ac_off).place(x=400, y=50)
+    padx=2, bg='plum1', command=vpn_ac_off).place(x=350, y=50)
 
 B14 = Button(root, font = 'arial 10 bold', text = "Auto-Connect On", 
-    padx=2, bg='SkyBlue1', command=vpn_ac_on).place(x=400, y=90)
+    padx=2, bg='plum1', command=vpn_ac_on).place(x=350, y=90)
 
 
 B15 = Button(root, font = 'arial 10 bold', text = "Quick Connect", 
-    padx=2, bg='violet red', command=vpn_c).place(x=200, y=50)
+    padx=2, bg='violet red', command=vpn_c).place(x=60, y=130)
 
 B16 = Button(root, font = 'arial 10 bold', text = "Connect - USA", 
-    padx=2, bg='PaleGreen2', command=vpn_c_us).place(x=200, y=90)
+    padx=2, bg='PaleGreen2', command=vpn_c_us).place(x=200, y=50)
 
 B17 = Button(root, font = 'arial 10 bold', text = "Connect - Chicago", 
-    padx=2, bg='PaleGreen2', command=vpn_c_chicago).place(x=200, y=130)
+    padx=2, bg='PaleGreen2', command=vpn_c_chicago).place(x=200, y=90)
 
 B18 = Button(root, font = 'arial 10 bold', text = "Connect - New York", 
-    padx=2, bg='PaleGreen2', command=vpn_c_new_york).place(x=200, y=170)
+    padx=2, bg='PaleGreen2', command=vpn_c_new_york).place(x=200, y=130)
 
 B19 = Button(root, font = 'arial 10 bold', text = "Connect - Miami", 
-    padx=2, bg='PaleGreen2', command=vpn_c_miami).place(x=200, y=210)
+    padx=2, bg='PaleGreen2', command=vpn_c_miami).place(x=200, y=170)
     
 B20 = Button(root, font = 'arial 10 bold', text = "Connect - Canada", 
-    padx=2, bg='yellow2', command=vpn_c_canada).place(x=200, y=250)
+    padx=2, bg='yellow2', command=vpn_c_canada).place(x=200, y=210)
 
-B21 = Button(root, font = 'arial 10 bold', text = "Connect - France", 
-    padx=2, bg='yellow2', command=vpn_c_france).place(x=200, y=290)
-
-B22 = Button(root, font = 'arial 10 bold', text = "Connect - Germany", 
-    padx=2, bg='yellow2', command=vpn_c_germany).place(x=200, y=330)
-    
 B23 = Button(root, font = 'arial 10 bold', text = "Connect - UK", 
-    padx=2, bg='yellow2', command=vpn_c_uk).place(x=200, y=370)
+    padx=2, bg='yellow2', command=vpn_c_uk).place(x=200, y=250)
     
         
 B24 = Button(root, font = 'arial 10 bold', text = "Disconnect VPN", 
-    padx=2, bg='orange red', command=vpn_disconnect).place(x=60, y=410)
+    padx=2, bg='orange red', command=vpn_disconnect).place(x=60, y=90)
 
     
 B25 = Button(root, font = 'arial 10 bold', text = "Remove Port 8080 Whitelist", 
-    padx=2, bg='ghost white', command=vpn_wl_off).place(x=600,
-        y=410)
+    padx=2, bg='ghost white', command=vpn_wl_off).place(x=650,
+        y=50)
     
 B26 = Button(root, font = 'arial 10 bold', text = "Add Port 8080 Whitelist", 
-    padx=2, bg='floral white', command=vpn_wl_on).place(x=600, 
-        y=450)
+    padx=2, bg='floral white', command=vpn_wl_on).place(x=650, 
+        y=90)
 
         
-B27 = Button(root, font = 'arial 10 bold', text = 'EXIT PROGRAM', width=18, 
-    command = Exit, bg = 'firebrick3', padx=2, pady=1).place(x=60, y=450)
+B27 = Button(root, font = 'arial 10 bold', text = 'EXIT', 
+    command = Exit, bg = 'firebrick3', padx=2, pady=1).place(x=60, y=50)
     
 
-B28 = Button(root, font = 'arial 10 bold', text = 'Clear Terminal', 
-    padx=2, bg='lime green', command=clear_terminal).place(x=400, y=450)    
+# Create tkinter text output widget
+text_output_window = Text(root, height=30, width=100, borderwidth=1, 
+    relief='ridge', bg='gray7', fg='lime green')
+text_output_window.place(x=50, y=400)
+
+# Create scrollbar for text output widget
+scrollbar = Scrollbar(root)
+scrollbar.place(x=900, y=650)
+
+# Configure commands for scrollbar and tie to text widget
+text_output_window.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=text_output_window.yview, bg='gray7', 
+    activebackground='lime green', highlightcolor='lime green', width=15)
+    
     
 root.mainloop()
 
